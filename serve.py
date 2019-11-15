435a87965bb1a1067c3ed33401e3d5da5e39727e
Garrett Smith <g@rre.tt>
2019-11-15 14:50:15 -0600

    Latest

--- serve.py
+++ serve.py
@@ -1,10 +1,18 @@
 from flask import Flask
 
+import h2o
+
 app = Flask(__name__)
+
+model_path = "output/model.h2o"
+
+h2o.init()
+model = h2o.load_model(model_path)
+print(model)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Make predictions with LOADED MODEL"
 
 if __name__ == '__main__':
     app.run()
